37dc5f9cc0f70a6a399cb464526916128e5947e6
Dmitrii Borovoi <dborovoy@nxsystemsinc.com>
2014-03-03 21:14:30 +1100

    (master) added pluginManagement section

--- Common/src/test/java/com/dimas/common/SpringAppTest.java
+++ Common/src/test/java/com/dimas/common/SpringAppTest.java
@@ -3,6 +3,8 @@ package com.dimas.common;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,12 +13,16 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-config.xml")
-public class SpringAppTests {
+public class SpringAppTest {
+
+    private final Logger l = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private HelloService helloService;
 
     @Test
     public void testSayHello() {
         assertEquals("Hello world!", helloService.sayHello());
+        l.info("Unit. Common");
     }
 }
